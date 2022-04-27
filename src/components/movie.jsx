364844ef2d44802de4609213d53cbc8e64c32505
Andy Nievas <nievasandy73@gmail.com>
2022-04-27 02:06:13 -0300

    fix blur on firefox

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -3,7 +3,7 @@ import './movie.css';
 import logo from "../assets/app-icon.jpg";
 import { Link } from "react-router-dom";
 
-function Spinner(){
+function Spinner() {
     return <div className="spinner-wrapper w-100 h-100 d-flex justify-content-center align-items-center" >
         <div className="spinner spinner-border text-primary" role="status">
             <span className="visually-hidden">Loading...</span>
@@ -11,34 +11,39 @@ function Spinner(){
     </div>
 }
 
-export default function Movie({ movie }){
+export default function Movie({ movie }) {
 
-    const [loading, setLoading] = useState( true );
+    const [loading, setLoading] = useState(true);
 
     const imgURL = movie.poster_path ?
-        `https://image.tmdb.org/t/p/w200${ movie.poster_path }`
+        `https://image.tmdb.org/t/p/w200${movie.poster_path}`
         : logo;
 
     return <div className="movie-card h-100" >
 
-        <Link to="/movie" className="text-light text-decoration-none" >
+        <Link to="/movie" className="w-100 text-light text-decoration-none" >
             <div className="visible-area h-100" >
-                <img className="w-100 h-100 rounded my-shadow" src={ imgURL } alt={movie.title} onLoad={ ()=> setLoading( false ) } />
-                <span className="yearOnImage small fw-bold bg-primary px-2 rounded-lg" >{ movie.release_date && movie.release_date.substring(0,4) }</span>
-                { loading && <Spinner /> }
+                <img className="w-100 h-100 rounded shadow" src={imgURL} alt={movie.title} onLoad={() => setLoading(false)} />
+                <span className="yearOnImage small fw-bold bg-primary px-2 rounded-lg" >{movie.release_date && movie.release_date.substring(0, 4)}</span>
+                {loading && <Spinner />}
             </div>
         </Link>
 
-        <div className="info h-100" >
-            <div className="imgFondo rounded my-auto" style={{ backgroundImage: `url(${ imgURL })` }} >
-                <div className="blur " >
-                    <h5 className="text-light fw-bold mt-0 mb-2 pb-1" >{ movie.title } </h5>
-                    <span className="text-warning fw-bold" ><span className="lead fw-bold" >{ movie.vote_average && movie.vote_average }/</span><span className="small" >10</span></span>
-                    <span className="mx-2 small" >{ movie.release_date && movie.release_date.substring(0,4) }</span>
-                    <span className="text-dark small fw-bold bg-warning px-1 rounded" >HD</span>
-                    <p className="overview fw-bolder mt-2 mb-0 p-0" >{ movie.overview ? movie.overview : "No hay descripcion"}</p>
+        <div className="info h-100 " >
+            <div className="position-relative" >
+
+                <div className="imgFondo rounded" >
+                    <img className="imgFondo blur rounded my-auto" src={imgURL} />
                 </div>
+                <div className="content p-4" >
+                    <h5 className="text-light fw-bold mt-0 mb-2 pb-1" >{movie.title} </h5>
+                    <span className="text-warning fw-bold" ><span className="lead fw-bold" >{movie.vote_average && movie.vote_average}/</span><span className="small" >10</span></span>
+                    <span className="mx-2 small" >{movie.release_date && movie.release_date.substring(0, 4)}</span>
+                    <span className="text-dark small fw-bold bg-warning px-1 rounded" >HD</span>
+                    <p className="overview fw-bolder mt-2 mb-0 p-0" >{movie.overview ? movie.overview : "No hay descripcion"}</p>
+                </div>
+
             </div>
         </div>
-    </div>   
+    </div>
 }
--- src/fetchMovies.js
+++ src/fetchMovies.js
@@ -1,27 +1,27 @@
 
 
-import key from "./data/key";
+// import key from "./data/key";
+const key = process.env.REACT_APP_API_KEY;
 
-
-async function fetchMovies( action, filter, page ){
+async function fetchMovies(action, filter, page) {
 
     let urlBase = `https://api.themoviedb.org/3/${action}/movie?api_key=${key}&include_adult=false&vote_count.gte=10`;
-    
-    if( action === "search" ){
+
+    if (action === "search") {
         urlBase += `&query=${filter}&page=${page}`
-    }else{
+    } else {
         urlBase += `&vote_average.gte=${filter}&page=${page}`
     }
 
-    try{
-        console.log( "Fetch url, page:", page );
-        console.log( "Fetch url:", urlBase );
-        const response = await fetch( urlBase );
+    try {
+        console.log("Fetch url, page:", page);
+        console.log("Fetch url:", urlBase);
+        const response = await fetch(urlBase);
         const data = await response.json();
         return data.results;
     }
-    catch (err){
-        console.log( err );
+    catch (err) {
+        console.log(err);
     }
 
 }
